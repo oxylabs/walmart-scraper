ea8ede42161250cc52c8735bba79805c37fdbc14
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2024-10-01 13:43:34 +0300

    Update curl_walmart.sh

--- code examples/curl_walmart.sh
+++ code examples/curl_walmart.sh
@@ -1,4 +1,4 @@
 curl --user user:pass \
 'https://realtime.oxylabs.io/v1/queries' \
 -H "Content-Type: application/json" \
--d '{"source": "universal_ecommerce", "url": "https://www.walmart.com/ip/Adidas-Moves-Body-Spray-for-Men-2-5-Oz/710726462", "geo_location": "United States","parse": true}'
+-d '{"source": "universal", "url": "https://www.walmart.com/ip/Adidas-Moves-Body-Spray-for-Men-2-5-Oz/710726462", "geo_location": "United States","parse": true}'
