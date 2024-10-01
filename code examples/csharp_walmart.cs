ac9fc51632c2315428fd87a79d16c3724825cd42
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2024-10-01 13:43:14 +0300

    Update csharp_walmart.cs

--- code examples/csharp_walmart.cs
+++ code examples/csharp_walmart.cs
@@ -15,7 +15,7 @@ namespace OxyApi
 
             var parameters = new Dictionary<string, string>()
             {
-                { "source", "universal_ecommerce" },
+                { "source", "universal" },
                 { "url", "https://www.walmart.com/ip/Adidas-Moves-Body-Spray-for-Men-2-5-Oz/710726462" },
                 { "geo_location" : "United States" },
                 { "parse", true },
